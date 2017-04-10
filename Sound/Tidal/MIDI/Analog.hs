4da5c6f82c5a32fe58cb3c3f494f30dab493cbac
sprite01 <YamatoWada@wadadaiwa-no-MacBook-Pro.local>
2017-04-11 00:45:58 +0900

    f l

--- Sound/Tidal/MIDI/Analog.hs
+++ Sound/Tidal/MIDI/Analog.hs
@@ -145,7 +145,7 @@ analogController = ControllerShape {controls = [
                             ],
                            -- duration = ("dur", 0.05),
                            -- velocity = ("vel", 0.5),
-                           latency = 0.07889}
+                           latency = 0.07989}
 
 
 analog = toShape analogController
